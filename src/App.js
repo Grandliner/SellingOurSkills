c85c650e1f893c6467e8e7f0b07ec8f88d4970ac
Grandliner <krasnikovitsme@gmail.com>
2022-01-12 18:53:32 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 
 const App = () => {
     return (
         <div className='app-wrapper' >
-           
-            
+            <Header />
+        
+
+         <header>
+             сайт для продажи услуг статика
+             </header>  
+            <ul>
+                <li>
+                    первая строчка из списка
+                </li>
+                <li>
+                    первая строчка из списка
+                </li>
+                <li>
+                    первая строчка из списка
+                </li>
+                <li>
+                    первая строчка из списка
+                </li>
+                <li>
+                    первая строчка из списка
+                </li>
+                <li>
+                    первая строчка из списка
+                </li>
+                <li>
+                    первая строчка из списка
+                </li>
+            </ul>
                
         </div>);
 }
